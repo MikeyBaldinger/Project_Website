249033d542cbd5f443cda0d621813f40fc2e3c00
MikeyBaldinger <54123064+MikeyBaldinger@users.noreply.github.com>
2020-05-20 12:25:20 -0700

    Updated search for player and team

--- stats.php
+++ stats.php
@@ -54,9 +54,13 @@
 			  $losses = $team['losses'];
 			  $ties = $team['ties'];
 
-     ?>
-       <li><?php echo "$name, $city, $coach, $wins, $losses, $ties"; ?></li>
-    <?php
+     ?>   
+	   <?php echo "$city"; ?> <?php echo "$name"; ?> <br>
+	   Coach: <?php echo "$coach"; ?> <br>
+	   Wins: <?php echo "$wins"; ?> <br>
+	   Losses: <?php echo "$losses"; ?> <br>
+	   Ties: <?php echo "$ties"; ?> <br>
+	   <?php
       }
 	  }
      ?>
@@ -74,10 +78,79 @@
 	  {
 		  while ($player = $player_results->fetch_assoc()) {
         $p_name = $player['player_name'];
-	$num = $player['player_num'];
-
+		$num = $player['player_num'];
+		$height = $player['height'];
+		$weight = $player['weight'];
+		$yrs_pro = $player['yrs_pro'];
+		$abbreviation = $player['abbreviation'];
      ?>
-       <li><?php echo "$p_name, $num"; ?></li>
+	 
+	<?php echo "$p_name"; ?> - #<?php echo "$num"; ?> <br>
+	Position: <?php echo "$abbreviation"; ?> <br>
+	Height: <?php echo "$height"; ?> <br>
+	Weight: <?php echo "$weight"; ?> <br>
+	Years Pro: <?php echo "$yrs_pro"; ?> <br>
+	
+    <?php
+      }
+	  }
+     ?>
+	 
+	  <?php
+
+      $user_input = $_POST["input"];
+
+      $query = "SELECT * ".
+               "FROM Quarterback ".
+	       "WHERE player_name = '$user_input' ";
+      $qb_results = $connection->query($query);
+	  if ($qb_results->num_rows > 0)
+	  {
+		  while ($qb = $qb_results->fetch_assoc()) {
+        $p_name = $qb['player_name'];
+		$pass_yards = $qb['pass_yards'];
+		$pass_tds = $qb['pass_tds'];
+		$interceptions = $qb['interceptions'];
+		$qb_rating = $qb['qb_rating'];
+     ?>
+	 
+	<br>
+	2019-2020 Stats <br>
+	Passing Yards: <?php echo "$pass_yards"; ?> <br>
+	Passing Touchdowns: <?php echo "$pass_tds"; ?> <br>
+	Interceptions: <?php echo "$interceptions"; ?> <br>
+	QB Rating: <?php echo "$qb_rating"; ?> <br>
+	
+    <?php
+      }
+	  }
+     ?>
+	 
+	   <?php
+
+      $user_input = $_POST["input"];
+
+      $query = "SELECT * ".
+               "FROM Runningback ".
+	       "WHERE player_name = '$user_input' ";
+      $rb_results = $connection->query($query);
+	  if ($rb_results->num_rows > 0)
+	  {
+		  while ($rb = $rb_results->fetch_assoc()) {
+        $p_name = $rb['player_name'];
+		$rush_yards = $rb['rush_yards'];
+		$touchdowns = $rb['touchdowns'];
+		$fumbles = $rb['fumbles'];
+		$carries = $rb['carries'];
+     ?>
+	 
+	<br>
+	2019-2020 Stats <br>
+	Rushing Yards: <?php echo "$rush_yards"; ?> <br>
+	Touchdowns: <?php echo "$touchdowns"; ?> <br>
+	Fumbles: <?php echo "$fumbles"; ?> <br>
+	Carries: <?php echo "$carries"; ?> <br>
+	
     <?php
       }
 	  }
