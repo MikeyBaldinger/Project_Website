db694113b577f654446dd5ae97cf404a1412aeed
MikeyBaldinger <54123064+MikeyBaldinger@users.noreply.github.com>
2020-05-20 22:53:42 -0700

    Updated

--- createPlayer.php
+++ createPlayer.php
@@ -1,25 +1,25 @@
 <?php
-  $username = "student";
-  $password = "CompSci364";
-  $database = "student";
-  $conn = new mysqli("localhost", $username, $password,
-                         $database);
+	$username = "student";
+	$password = "CompSci364";
+	$database = "student";
+	$conn = new mysqli("localhost", $username, $password, $database);
 ?>
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>createPlayer</title>
-    <link rel="stylesheet" href="nfl.css" />
-  </head>
-  <body>
-    <h1><img src="NFL_logo.jpg" height="125" width="125">Skill Players Database</h1>
+	<head>
+		<meta charset="utf-8">
+		<title>createPlayer</title>
+		<link rel="stylesheet" href="nfl.css" />
+	</head>
+	<body>
+		
+		<h1><img src="NFL_logo.jpg" height="125" width="125">Skill Players Database</h1>
 
-    <div class="topnav">
-      <a href="main.html">Home</a>
-      <a href="about.html">About</a>
-      <a href="stats.php">Stats</a>
+		<div class="topnav">
+		<a href="main.html">Home</a>
+		<a href="about.html">About</a>
+		<a href="stats.php">Stats</a>
 			<div class="dropdown">
 				<button class="dbutton" name="dbutton">Create
 					<i class="fa fa-caret-down"></i>
@@ -29,35 +29,33 @@
 					<a href="createTeam.php">Team</a>
 				</div>
 			</div>
-    </div>
+		</div>
 
-    <h2>Create a Player</h2>
+		<h2>Create a Player</h2>
 
-    <form class="newplayer" id="newplayer" action="createPlayer.php" method="post"
-          onsubmit="main.html">
-      <label for="pname">Player Name</label>
-      <input type="text" name="pname" id="pname" required /><br />
-      <label for="tname">Team Name</label>
-      <input type="text" name="tname" id="tname" required /><br />
-      <label for="num">Player Number</label>
-      <input type="number" name="num" id="num" min="0" max="99" required /><br />
-      <label for="height">Height (ft-in)</label>
-      <input type="text" name="height" id="height" placeholder="6-3" required /><br />
-      <label for="weight">Weight</label>
-      <input type="number" name="weight" id="weight" required /><br />
-      <label for="pos">Position</label>
-      <input type="radio" name="pos" value="QB" required>QB
-      <input type="radio" name="pos" value="RB">RB
-      <input type="radio" name="pos" value="WR">WR
-      <input type="radio" name="pos" value="TE">TE
-      <br><br>
+		<form class="newplayer" id="newplayer" action="createPlayer.php" method="post" onsubmit="main.html">
+			<label for="pname">Player Name</label>
+			<input type="text" name="pname" id="pname" required /><br />
+			<label for="tname">Team Name</label>
+			<input type="text" name="tname" id="tname" required /><br />
+			<label for="num">Player Number</label>
+			<input type="number" name="num" id="num" min="0" max="99" required /><br />
+			<label for="height">Height (ft-in)</label>
+			<input type="text" name="height" id="height" placeholder="6-3" required /><br />
+			<label for="weight">Weight</label>
+			<input type="number" name="weight" id="weight" required /><br />
+			<label for="pos">Position</label>
+			<input type="radio" name="pos" value="QB" required>QB
+			<input type="radio" name="pos" value="RB">RB
+			<input type="radio" name="pos" value="WR">WR
+			<input type="radio" name="pos" value="TE">TE
+			<br>
+			<br>
 
+			<input type="submit" value="Submit" />
+		</form>
 
-
-      <input type="submit" value="Submit" />
-    </form>
-
-  </body>
+	</body>
 </html>
 
 <?php
@@ -75,8 +73,21 @@ if (isset($_POST['pname']))  {
     			echo "Error: ".$conn->error."\n";
   		}
 	}
+	
+	if ($pos == "QB") {
+		include 'createQB.php';
+	}
+	else if ($pos == "RB") {
+		include 'createRB.php';
+	}
+	else if ($pos == "WR") {
+		include 'createWR.php';
+	}
+	else {
+		include 'createTE.php';
+	}
 }
-
+	
 $conn->close();
 
 ?>
