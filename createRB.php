a76ed24ccd6a4f0578d03cd821237363a74cafb0
MikeyBaldinger <54123064+MikeyBaldinger@users.noreply.github.com>
2020-05-20 22:56:54 -0700

    New

---
+++ createRB.php
@@ -0,0 +1,48 @@
+<?php
+	$username = "student";
+	$password = "CompSci364";
+	$database = "student";
+	$conn = new mysqli("localhost", $username, $password, $database);
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+    <h2>Create a RB</h2>
+
+    <form class="newRB" id="newRB" action="createRB.php" method="post" onsubmit="main.html">
+		<label for="playerName">Player Name</label>
+		<input type="text" name="playerName" id="playerName" required /><br />
+		<label for="rushYards">Rushing Yards</label>
+		<input type="number" name="rushYards" id="rushYards" required /><br />
+		<label for="tds">Touchdowns</label>
+		<input type="number" name="tds" id="tds" required /><br />
+		<label for="fumbles">Fumbles</label>
+		<input type="number" name="fumbles" id="fumbles" required /><br />
+		<label for="carries">Carries</label>
+		<input type="number" name="carries" id="carries" required /><br />
+		<br>
+
+		<input type="submit" value="Submit" />
+    </form>
+
+</html>
+
+<?php
+
+//variables when new player is created
+if (isset($_POST['rushYards']))  {
+	$playerName = $_POST['playerName'];
+	$rushYards = $_POST['rushYards'];
+	$tds = $_POST['tds'];
+	$fumbles = $_POST['fumbles'];
+	$carries = $_POST['carries'];
+	$rb_insert = array( "INSERT INTO Runningback (player_name, rush_yards, touchdowns, fumbles, carries) VALUES ('$playerName', '$rushYards', '$tds', '$fumbles', '$carries'); ",);
+	foreach ($rb_insert as $query) {
+  		if (! $conn->query($query)) {
+    			echo "Error: ".$conn->error."\n";
+  		}
+	}
+}
+	
+?>
