e739020af18718179736e286b04e987e30f842f8
cadenkulp22 <54109914+cadenkulp22@users.noreply.github.com>
2020-05-20 21:17:15 -0600

    Update stats.php

--- stats.php
+++ stats.php
@@ -35,7 +35,7 @@
 
     <h2>Stats</h2>
   <ul>
-	 
+
 	 <?php
 
       $team_input = $_POST["input"];
@@ -54,17 +54,42 @@
 			  $losses = $team['losses'];
 			  $ties = $team['ties'];
 
-     ?>   
+     ?>
 	   <?php echo "$city"; ?> <?php echo "$name"; ?> <br>
 	   Coach: <?php echo "$coach"; ?> <br>
 	   Wins: <?php echo "$wins"; ?> <br>
 	   Losses: <?php echo "$losses"; ?> <br>
-	   Ties: <?php echo "$ties"; ?> <br>
+	   Ties: <?php echo "$ties"; ?> <br><br>
 	   <?php
       }
 	  }
-     ?>
+	  ?>
 
+<?php
+
+      $team_input = $_POST["input"];
+
+		if ($team_input == "roster" || $team_input == "Roster") {
+      $roster_query = "SELECT * ".
+               "FROM Roster ";
+      $roster_results = $connection->query($roster_query);
+	  if ($roster_results->num_rows > 0)
+	  {
+		  while ($roster = $roster_results->fetch_assoc()) {
+
+			  $player_name = $roster['player_name'];
+			  $team_name = $roster['team_name'];
+
+     ?>
+	   --------------------------------------------------<br>
+	   Name: <?php echo "$player_name"; ?> <br>
+	   Team: <?php echo "$team_name"; ?> <br>
+	   <?php
+
+      }
+	  }
+		}
+     ?>
 
     <?php
 
@@ -84,18 +109,18 @@
 		$yrs_pro = $player['yrs_pro'];
 		$abbreviation = $player['abbreviation'];
      ?>
-	 
+
 	<?php echo "$p_name"; ?> - #<?php echo "$num"; ?> <br>
 	Position: <?php echo "$abbreviation"; ?> <br>
 	Height: <?php echo "$height"; ?> <br>
 	Weight: <?php echo "$weight"; ?> <br>
 	Years Pro: <?php echo "$yrs_pro"; ?> <br>
-	
+
     <?php
       }
 	  }
      ?>
-	 
+
 	  <?php
 
       $user_input = $_POST["input"];
@@ -113,19 +138,19 @@
 		$interceptions = $qb['interceptions'];
 		$qb_rating = $qb['qb_rating'];
      ?>
-	 
+
 	<br>
 	2019-2020 Stats <br>
 	Passing Yards: <?php echo "$pass_yards"; ?> <br>
 	Passing Touchdowns: <?php echo "$pass_tds"; ?> <br>
 	Interceptions: <?php echo "$interceptions"; ?> <br>
 	QB Rating: <?php echo "$qb_rating"; ?> <br>
-	
+
     <?php
       }
 	  }
      ?>
-	 
+
 	 <?php
 
       $user_input = $_POST["input"];
@@ -143,19 +168,19 @@
 		$fumbles = $rb['fumbles'];
 		$carries = $rb['carries'];
      ?>
-	 
+
 	<br>
 	2019-2020 Stats <br>
 	Rushing Yards: <?php echo "$rush_yards"; ?> <br>
 	Touchdowns: <?php echo "$touchdowns"; ?> <br>
 	Fumbles: <?php echo "$fumbles"; ?> <br>
 	Carries: <?php echo "$carries"; ?> <br>
-	
+
     <?php
       }
 	  }
      ?>
-	 
+
 	 <?php
 
       $user_input = $_POST["input"];
@@ -171,19 +196,20 @@
 		$rec_yds = $wr['rec_yards'];
 		$rec_tds = $wr['rec_tds'];
 		$receptions = $wr['receptions'];
+
      ?>
-	 
+
 	<br>
 	2019-2020 Stats <br>
 	Receiving Yards: <?php echo "$rec_yds"; ?> <br>
 	Receiving Touchdowns: <?php echo "$rec_tds"; ?> <br>
 	Receptions: <?php echo "$receptions"; ?> <br>
-	
+
     <?php
       }
 	  }
      ?>
-	 
+
 	 <?php
 
       $user_input = $_POST["input"];
@@ -200,13 +226,127 @@
 		$rec_tds = $te['rec_tds'];
 		$receptions = $te['receptions'];
      ?>
-	 
+
 	<br>
 	2019-2020 Stats <br>
 	Receiving Yards: <?php echo "$rec_yds"; ?> <br>
 	Receiving Touchdowns: <?php echo "$rec_tds"; ?> <br>
 	Receptions: <?php echo "$receptions"; ?> <br>
-	
+
+    <?php
+      }
+	  }
+     ?>
+
+
+	 <?php
+
+      $user_input = $_POST["input"];
+
+      $team_query = "SELECT * ".
+		"FROM Quarterback INNER JOIN Roster USING (player_name) ".
+		"WHERE Roster.team_name = '$user_input' ";
+      $q_results = $connection->query($team_query);
+	  if ($q_results->num_rows > 0)
+	  {
+		  while ($q = $q_results->fetch_assoc()) {
+			  $pname = $q['player_name'];
+			  $pass_yards = $q['pass_yards'];
+			  $pass_tds = $q['pass_tds'];
+			  $int = $q['interceptions'];
+			  $rating = $q['qb_rating'];
+     ?>
+	<br>2019-2020 Player Stats <br><br>
+
+	<?php echo "$pname"; ?> <br>
+	Passing Yards: <?php echo "$pass_yards"; ?> <br>
+	Passing Touchdowns: <?php echo "$pass_tds"; ?> <br>
+	Interceptions: <?php echo "$int"; ?> <br>
+	QB Rating: <?php echo "$rating"; ?> <br>
+
+    <?php
+      }
+	  }
+     ?>
+
+
+	 <?php
+
+      $user_input = $_POST["input"];
+
+      $team_query = "SELECT * ".
+		"FROM Runningback INNER JOIN Roster USING (player_name) ".
+		"WHERE Roster.team_name = '$user_input' ";
+      $q_results = $connection->query($team_query);
+	  if ($q_results->num_rows > 0)
+	  {
+		  while ($q = $q_results->fetch_assoc()) {
+			  $pname = $q['player_name'];
+			  $run_yards = $q['rush_yards'];
+			  $run_tds = $q['touchdowns'];
+			  $car = $q['carries'];
+			  $fum = $q['fumbles'];
+     ?>
+	<br><?php echo "$pname"; ?> <br>
+	Rushing Yards: <?php echo "$run_yards"; ?> <br>
+	Rushing Touchdowns: <?php echo "$run_tds"; ?> <br>
+	Carries: <?php echo "$car"; ?> <br>
+	Fumbles: <?php echo "$fum"; ?> <br>
+
+    <?php
+      }
+	  }
+     ?>
+
+
+	 <?php
+
+      $user_input = $_POST["input"];
+
+      $team_query = "SELECT * ".
+		"FROM Widereceiver INNER JOIN Roster USING (player_name) ".
+		"WHERE Roster.team_name = '$user_input' ";
+      $q_results = $connection->query($team_query);
+	  if ($q_results->num_rows > 0)
+	  {
+		  while ($q = $q_results->fetch_assoc()) {
+			  $pname = $q['player_name'];
+			  $recyards = $q['rec_yards'];
+			  $tds = $q['rec_tds'];
+			  $rec = $q['receptions'];
+     ?>
+	<br><?php echo "$pname"; ?> <br>
+	Receiving Yards: <?php echo "$recyards"; ?> <br>
+	Receiving Touchdowns: <?php echo "$tds"; ?> <br>
+	Receptions: <?php echo "$rec"; ?> <br>
+
+    <?php
+      }
+	  }
+     ?>
+
+
+	 <?php
+
+      $user_input = $_POST["input"];
+
+      $team_query = "SELECT * ".
+		"FROM Tightend INNER JOIN Roster USING (player_name) ".
+		"WHERE Roster.team_name = '$user_input' ";
+      $q_results = $connection->query($team_query);
+	  if ($q_results->num_rows > 0)
+	  {
+		  while ($q = $q_results->fetch_assoc()) {
+			  $pname = $q['player_name'];
+			  $recyards = $q['rec_yards'];
+			  $tds = $q['rec_tds'];
+			  $rec = $q['receptions'];
+     ?>
+	<br><?php echo "$pname"; ?> <br>
+	Receiving Yards: <?php echo "$recyards"; ?> <br>
+	Receiving Touchdowns: <?php echo "$tds"; ?> <br>
+	Receptions: <?php echo "$rec"; ?> <br>
+
     <?php
       }
 	  }
